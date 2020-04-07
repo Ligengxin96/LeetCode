2beef2c99b65c1f0b92c80e06c2971df40ad422c
liwenbin <ligengxin96@gmail.com>
2020-04-07 22:45:34 +0800

    解决 104 题

---
+++ src/MaximumDepthOfBinaryTree_0104/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,25 @@
+package MaximumDepthOfBinaryTree_0104;
+
+/**
+ * @author : Mr.Li
+ * @version : V1.0
+ * @packageName :  MaximumDepthOfBinaryTree_0104
+ * @created : 2020/4/7
+ * @description : https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ */
+public class MaximumDepthOfBinaryTree {
+
+    public static int maxDepth(TreeNode root) {
+        if(root != null) {
+            int rightMaxDepth = maxDepth(root.right) + 1;
+            int leftMaxDepth = maxDepth(root.left) + 1;
+            return rightMaxDepth > leftMaxDepth ? rightMaxDepth : leftMaxDepth;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        // 测试用例太麻烦了 就不写了 这个方法复制上去可以直接使用
+    }
+}
